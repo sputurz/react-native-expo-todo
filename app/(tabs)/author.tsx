8e7ce58bc93fe7f5bee303bb9a6f5ef5783494c3
sputurz <sputurza@gmail.com>
2025-12-24 14:55:12 +0300

    24.12.25h14

--- app/(tabs)/author.tsx
+++ app/(tabs)/author.tsx
@@ -8,7 +8,7 @@ import {
   View,
 } from 'react-native';
 
-export default function StatsScreen() {
+export default function About() {
   const openGitHub = () => {
     Linking.openURL('https://github.com/sputurz/');
   };
